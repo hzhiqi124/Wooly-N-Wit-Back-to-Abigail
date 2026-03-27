e55bded81492c38c7cf3bc2785a98ccaebb7fb95
heavypause <HO0004QI@e.ntu.edu.sg>
2026-03-27 16:00:40 +0800

    PlayerController Setup

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,42 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float jumpForce = 10f;
+
+    private Rigidbody2D rb;
+    private InputAction moveAction;
+    private InputAction jumpAction;
+    private bool isGrounded;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        PlayerInput playerInput = GetComponent<PlayerInput>();
+        moveAction = playerInput.actions["Move"];
+        jumpAction = playerInput.actions["Jump"];
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float move = moveAction.ReadValue<Vector2>().x;
+        rb.linearVelocity = new Vector2(move * moveSpeed, rb.linearVelocity.y);
+
+        if (jumpAction.WasPressedThisFrame() && isGrounded)
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag("Platform"))
+            isGrounded = true;
+    }
+
+    void OnCollisionExit2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag("Platform"))
+            isGrounded = false;
     }
 }
