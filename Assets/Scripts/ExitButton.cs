f492f741d63d0f2f488dcad591f53a72c9bcadc3
heavypause <HO0004QI@e.ntu.edu.sg>
2026-04-07 00:51:53 +0800

    BGM, scene button SFX

--- Assets/Scripts/ExitButton.cs
+++ Assets/Scripts/ExitButton.cs
@@ -5,13 +5,23 @@ using ChristinaCreatesGames.Animations;
 public class ExitButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
 {
     private SquashAndStretch squashAndStretch;
+    private AudioSource audioSource;
+    [SerializeField] private AudioClip hoverSound;
+    [SerializeField] private AudioClip clickSound;
 
     void Start()
     {
         squashAndStretch = GetComponent<SquashAndStretch>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     public void OnPointerClick(PointerEventData eventData)
+    {
+        audioSource.PlayOneShot(clickSound);
+        Invoke("ExitScene", 0.2f);
+    }
+
+    void ExitScene()
     {
         SceneController.instance.LoadScene("MenuScene");
     }
@@ -19,6 +29,7 @@ public class ExitButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandl
     public void OnPointerEnter(PointerEventData eventData)
     {
         squashAndStretch.PlaySquashAndStretch();
+        audioSource.PlayOneShot(hoverSound);
     }
 
     public void OnPointerExit(PointerEventData eventData)
--- Assets/Scripts/RedoButton.cs
+++ Assets/Scripts/RedoButton.cs
@@ -5,13 +5,23 @@ using ChristinaCreatesGames.Animations;
 public class RedoButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
 {
     private SquashAndStretch squashAndStretch;
+    private AudioSource audioSource;
+    [SerializeField] private AudioClip hoverSound;
+    [SerializeField] private AudioClip clickSound;
 
     void Start()
     {
         squashAndStretch = GetComponent<SquashAndStretch>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     public void OnPointerClick(PointerEventData eventData)
+    {
+        audioSource.PlayOneShot(clickSound);
+        Invoke("ReloadScene", 0.2f);
+    }
+    
+    void ReloadScene()
     {
         SceneController.instance.LoadScene(
             UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
@@ -20,6 +30,7 @@ public class RedoButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandl
     public void OnPointerEnter(PointerEventData eventData)
     {
         squashAndStretch.PlaySquashAndStretch();
+        audioSource.PlayOneShot(hoverSound);
     }
 
     public void OnPointerExit(PointerEventData eventData)
