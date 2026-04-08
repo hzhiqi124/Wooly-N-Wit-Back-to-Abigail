0ea4026e81dec8c7bbc559d553badd372ea08d71
heavypause <HO0004QI@e.ntu.edu.sg>
2026-04-08 20:57:17 +0800

    star light

--- Assets/Scripts/Star.cs
+++ Assets/Scripts/Star.cs
@@ -3,12 +3,14 @@ using UnityEngine;
 public class Star : MonoBehaviour
 {
     [SerializeField] private GameObject finishPointWall;
+    [SerializeField] private GameObject lightWall;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
             finishPointWall.SetActive(true);
+            lightWall.SetActive(true);
             gameObject.SetActive(false);
         }
     }
