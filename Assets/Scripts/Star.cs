88a295cba32e8ab62519fa6741db39bddd15ca9e
heavypause <HO0004QI@e.ntu.edu.sg>
2026-04-13 20:42:26 +0800

    tutorial bubbles

--- Assets/Scripts/Star.cs
+++ Assets/Scripts/Star.cs
@@ -5,6 +5,8 @@ public class Star : MonoBehaviour
     [SerializeField] private GameObject finishPointWall;
     [SerializeField] private GameObject lightWall;
     [SerializeField] AudioClip starSound;
+    [SerializeField] public GameObject Tutorial01;
+    [SerializeField] public GameObject Tutorial02;
 
     private AudioSource audioSource;
 
@@ -21,11 +23,19 @@ public class Star : MonoBehaviour
             finishPointWall.SetActive(true);
             lightWall.SetActive(true);
             Invoke("DisableStar", 0.5f);
+            Invoke("EnableTutorial", 0.5f);
+            
         }
     }
 
     void DisableStar()
     {
         gameObject.SetActive(false);
+    }
+
+    void EnableTutorial()
+    {
+        Tutorial01.SetActive(true);
+        Tutorial02.SetActive(true);
     }
 }
